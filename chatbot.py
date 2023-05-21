c8ab68e4a9aa98eeabb217226b165d2ec7e42ebb
Lucien Shui <lucien@lucien.ink>
2023-05-21 16:39:37 +0800

    support bot map

--- chatbot.py
+++ chatbot.py
@@ -1,5 +1,7 @@
 from requests.api import post
-import os
+from json import load
+from urllib.parse import urljoin
+from typing import Dict
 
 
 class ChatBotBase:
@@ -30,10 +32,11 @@ class ChatBotBase:
 
 
 class ChatGPT(ChatBotBase):
-    def __init__(self, api_key: str, endpoint: str = 'https://api.openai-proxy.com',
-                 model: str = "gpt-3.5-turbo"):
+    def __init__(self, api_key: str, endpoint: str = 'https://api.openai-proxy.com', model: str = "gpt-3.5-turbo"):
+        self.model_list: list = ["gpt-4-0314", "gpt-4", "gpt-3.5-turbo-0301", "gpt-3.5-turbo"]
+        assert model in self.model_list
         self.api_key: str = api_key
-        self.url: str = os.path.join(endpoint, '/v1/chat/completions')
+        self.url: str = urljoin(endpoint, '/v1/chat/completions')
         self.model: str = model
         self.headers = {'Authorization': f'Bearer {self.api_key}'}
 
@@ -47,3 +50,18 @@ class ChatGPT(ChatBotBase):
         response = post(self.url, json={"model": self.model, "messages": messages}, headers=self.headers).json()
         message: str = response['choices'].pop(0)['message']['content']
         return message
+
+
+def from_config(bot_class: str, config: dict) -> ChatBotBase:
+    supported_class = ['chatgpt']
+    assert bot_class in supported_class
+    if bot_class in 'chatgpt':
+        return ChatGPT(**config)
+
+
+def from_bot_map_config(bot_map_config: Dict[str, dict]) -> Dict[str, ChatBotBase]:
+    bot_map: Dict[str, ChatBotBase] = {}
+    for bot, config in bot_map_config.items():
+        bot_class = config.pop('class')
+        bot_map[bot] = from_config(bot_class, config)
+    return bot_map
--- main.py
+++ main.py
@@ -1,9 +1,12 @@
-from flask import Flask, request, jsonify
-from chatbot import ChatGPT
-import os
 import logging
-from json import dumps
+import os
 from functools import partial
+from json import dumps
+
+from flask import Flask, request, jsonify
+
+from chatbot import from_bot_map_config
+from util import load_config
 
 dumps = partial(dumps, ensure_ascii=False, separators=(',', ':'))
 
@@ -13,23 +16,24 @@ logging.basicConfig(level=logging.INFO, format='[%(asctime)s] %(levelname)s:%(na
                     datefmt='%Y-%m-%d %H:%M:%S')
 logger: logging.Logger = logging.getLogger('robot')
 
-api_key: str = os.environ['OPENAI_API_KEY']
-model: str = os.environ['OPENAI_MODEL']
-bot = ChatGPT(api_key=api_key, model=model)
+config = load_config(os.environ['CONFIG_FILE'])
+bot_map = from_bot_map_config(config['bot_map'])
+token_list: list = config['token_list']
 
 
-@app.route('/api/robot/chat', methods=['POST'])
-def chat_api():
+@app.route('/api/chat/<bot>', methods=['POST'])
+def chat_api(bot: str):
+    token: str = request.headers['Token']
+    assert token in token_list
     body: dict = request.get_json()
     logger.debug(dumps(body))
-    json_response = bot.process(body)
+    json_response = bot_map[bot].process(body)
     logger.debug(dumps(json_response))
-
     return jsonify({})
 
 
 def main():
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=8000)
 
 
 if __name__ == '__main__':
